3addfdd5de763bf3dd32d52062db187100a20d51
Farai Mathemera <mathemerafarai@gmail.com>
2022-08-14 16:00:44 +0200

    Components skeleton
    A few more components added. Not fleshed out.

--- bitcube-noms/src/app/app.module.ts
+++ bitcube-noms/src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeLoaderComponent } from './components/home-loader/home-loader.component';
+import { OrderPageComponent } from './pages/order-page/order-page.component';
+import { ServerPageComponent } from './pages/server-page/server-page.component';
+import { MobileTrackingComponent } from './pages/mobile-tracking-page/mobile-tracking.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AssistantComponent } from './components/assistant/assistant.component';
+import { MenuComponent } from './components/menu/menu.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'mobile', component: MobileTrackingComponent },
+  { path: 'order', component: OrderPageComponent },
+  { path: 'despatch', component: ServerPageComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
-    HomeLoaderComponent
+    HomeLoaderComponent,
+    OrderPageComponent,
+    ServerPageComponent,
+    MobileTrackingComponent,
+    HomePageComponent,
+    AssistantComponent,
+    MenuComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), FlexLayoutModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ bitcube-noms/src/app/pages/mobile-tracking-page/mobile-tracking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-tracking',
+  templateUrl: './mobile-tracking.component.html',
+  styleUrls: ['./mobile-tracking.component.scss']
+})
+export class MobileTrackingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
