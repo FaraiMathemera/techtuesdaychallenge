bfce83f3dc24ba62e036da3e46b8ea8d265deae3
Farai Mathemera <mathemerafarai@gmail.com>
2022-08-15 13:34:58 +0200

    Typings for items

--- bitcube-noms/src/app/pages/order-page/order-page.component.ts
+++ bitcube-noms/src/app/pages/order-page/order-page.component.ts
@@ -8,16 +8,91 @@ import { Component, OnInit } from '@angular/core';
 export class OrderPageComponent implements OnInit {
   total: number = 0;
   tax: number = 0;
-  items: any[] = [
-    { name: 'Bacon & Cheese Burger', count: 1, total: 120.5 },
-    { name: 'Beef Burger', count: 1, total: 120.0 },
-    { name: 'Chicken Burger', count: 1, total: 120.5 },
-    // { name: 'Chicken Wings (8pcs)', count: 1, total: 120.0 },
-    // { name: 'Chicken Wings (12pcs)', count: 1, total: 120.0 },
-    // { name: 'Chips (Large)', count: 1, total: 120.0 },
-    // { name: 'Chips (Medium)', count: 1, total: 120.0 },
-    // { name: 'Coke', count: 1, total: 120.5 },
-    // { name: 'Fanta', count: 1, total: 120.0 },
+
+  // Item counts
+  baconCheeseBurgerCount = 0;
+  beefBurgerCount = 0;
+  chickenBurgerCount = 0;
+  wings8Count = 0;
+  wings12Count = 0;
+  chipsLargeCount = 0;
+  chipsMediumCount = 0;
+  cokeCount = 0;
+  fantaCount = 0;
+
+  // Item prices
+  prices: any = {
+    baconCheeseBurger: { price: 120 },
+    beefBurger: { price: 100 },
+    chickenBurger: { price: 95 },
+    wings8: { price: 50 },
+    wings12: { price: 80 },
+    chipsLarge: { price: 35 },
+    chipsMedium: { price: 20 },
+    soda: { price: 15 },
+  };
+
+  // Invoice items
+  items: {
+    name: string;
+    count: number;
+    total: number;
+    notes?: string | null;
+  }[] = [
+    {
+      name: 'Bacon & Cheese Burger',
+      count: this.baconCheeseBurgerCount,
+      total: this.baconCheeseBurgerCount * this.prices.baconCheeseBurger,
+      notes: null,
+    },
+    {
+      name: 'Beef Burger',
+      count: this.beefBurgerCount,
+      total: this.beefBurgerCount * this.prices.beefBurger,
+      notes: null,
+    },
+    {
+      name: 'Chicken Burger',
+      count: this.chickenBurgerCount,
+      total: this.chickenBurgerCount * this.prices.chickenBurger,
+      notes: null,
+    },
+    {
+      name: 'Chicken Wings (8pcs)',
+      count: this.wings8Count,
+      total: this.wings8Count * this.prices.wings8,
+      notes: null,
+    },
+    {
+      name: 'Chicken Wings (12pcs)',
+      count: this.wings12Count,
+      total: this.wings12Count * this.prices.wings12,
+      notes: null,
+    },
+    {
+      name: 'Chips (Large)',
+      count: this.chipsLargeCount,
+      total: this.chipsLargeCount * this.prices.chipsLarge,
+      notes: null,
+    },
+    {
+      name: 'Chips (Medium)',
+      count: this.chipsMediumCount,
+      total: this.chipsMediumCount * this.prices.chipsMedium,
+      notes: null,
+    },
+    {
+      name: 'Coke',
+      count: this.cokeCount,
+      total: this.cokeCount * this.prices.soda,
+      notes: null,
+    },
+    {
+      name: 'Fanta',
+      count: this.fantaCount,
+      total: this.fantaCount * this.prices.soda,
+      notes: null,
+    },
   ];
 
   constructor() {}
